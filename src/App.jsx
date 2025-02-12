4de815782c4f45e171a6f8b2ee9fc0c0d595cb99
WenHao Wu <wenhaowu1999@gmail.com>
2025-02-12 17:13:05 -0500

    Add page view tracking with GA4 using dataLayer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 
@@ -14,6 +15,17 @@ function App() {
     "Basic GPA Calculator",
     "Rock-Paper-Scissors",
   ];
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        event: "page_view",
+        page_path: location.pathname + location.search,
+      });
+    }
+  }, [location]);
 
   return (
     <div className="App">
